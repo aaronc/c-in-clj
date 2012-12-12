1748f8c840ab64f5aa71c5889ddd8919ca2a8b5c
Aaron Craelius <aaroncraelius@gmail.com>
2012-12-11 19:11:12 -0500

    Work on elf loading and remote linking.

--- src/c_in_clj/gcc/elf.clj
+++ src/c_in_clj/gcc/elf.clj
@@ -3,7 +3,7 @@
    [System.IO Stream SeekOrigin
     EndOfStreamException FileStream
     FileMode FileAccess FileShare
-    MemoryStream]))
+    MemoryStream Path]))
 
 ;; Byte Buffer Functions
 
@@ -104,28 +104,48 @@ specified position."
 (defreader read-u64 8 [bytes index] (BitConverter/ToUInt64 bytes index))
 (defreader read-i64 8 [bytes index] (BitConverter/ToInt64 bytes index))
 
-(defn- elf-type->reader32 [type & {:keys [len]}]
-  (case type
-    :uchar (fn [] (brbytes len))
-    :uint16_t read-u16
-    :uint32_t read-u32
-    :ElfN_Addr read-u32
-    :ElfN_Off read-u32))
+(defn- make-enum [& kvps]
+  (let [res (apply hash-map kvps)
+        val-map (zipmap (vals res) (keys res))]
+    (with-meta res {:val-map val-map})))
+
+(defmacro elfenum [name & kvps]
+  `(def ~name (make-enum ~@kvps)))
+
+(defn- to-enum-name [enum value]
+  (let [{:keys [val-map]} (meta enum)]
+    (or (get val-map value) value)))
+
+(defn- elf-type->reader32 [type {:keys [len enum convert]}]
+  (let [enum (eval enum)
+        convert (eval convert)
+        reader
+        (case type
+          :uchar (if len (fn [] (brbytes len)) brbyte)
+          :uint16_t read-u16
+          :uint32_t read-u32
+          :int32_t read-i32
+          :ElfN_Addr read-u32
+          :ElfN_Off read-u32)
+        reader (if enum
+                 (fn read-enum [] (to-enum-name enum (reader)))
+                 reader)
+        reader (if convert
+                 (fn read-convert [] (convert (reader)))
+                 reader)]
+    reader))
 
 (def ^:private elf32-structs (atom {}))
 (def ^:private elf64-structs (atom {}))
 
-(defmacro elf-enum [name val-map]
-  `(def ~name ~val-map))
-
 (defn- compile-elf-struct [name forms arch]
   (let [fields
         (doall
-         (for [[type name len] forms]
+         (for [[type name & {:keys [len] :as opts}] forms]
            (let [name (keyword name)
                  type (keyword type)
                  reader (case arch
-                          32 (elf-type->reader32 type :len len))]
+                          32 (elf-type->reader32 type opts))]
              {:type type :name name :len len
               :reader reader})))]
     (case arch
@@ -162,7 +182,7 @@ specified position."
 
 (elf-structN
  Ehdr
- (uchar e_ident 16) ;;  Magic number and other info 
+ (uchar e_ident :len 16) ;;  Magic number and other info 
  (uint16_t    e_type) ;;  Object file type 
  (uint16_t    e_machine) ;;  Architecture 
  (uint32_t    e_version) ;;  Object file version 
@@ -188,10 +208,37 @@ specified position."
  (uint32_t    p_flags) ;;  Segment flags 
  (uint32_t    p_align)) ;;  Segment alignment 
 
+(elfenum sh_type_enum
+         :SHT_NULL 0
+         :SHT_PROGBITS 1
+         :SHT_SYMTAB 2
+         :SHT_STRTAB 3
+         :SHT_RELA 4
+         :SHT_HASH 5
+         :SHT_DYNAMIC 6
+         :SHT_NOTE 7
+         :SHT_NOBITS 8
+         :SHT_REL 9
+         :SHT_SHLIB 10
+         :SHT_DYNSYM 11
+         :SHT_LOPROC 0x70000000
+         :SHT_HIPROC 0x7fffffff
+         :SHT_LOUSER 0x80000000
+         :SHT_HIUSER 0xffffffff )
+
+(elfenum shnum_special_enum
+         :SHN_UNDEF 0
+         :SHN_LORESERVE 0xff00
+         :SHN_LOPROC 0xff00
+         :SHN_HIPROC 0xff1f
+         :SHN_ABS 0xfff1
+         :SHN_COMMON 0xfff2
+         :SHN_HIRESERVE 0xffff)
+
 (elf-struct32
  Shdr
  (uint32_t    sh_name) ;;  Section name (string tbl index) 
- (uint32_t    sh_type) ;;  Section type 
+ (uint32_t    sh_type :enum sh_type_enum) ;;  Section type 
  (uint32_t    sh_flags) ;;  Section flags 
  (ElfN_Addr    sh_addr) ;;  Section virtual addr at execution 
  (ElfN_Off     sh_offset) ;;  Section file offset 
@@ -200,6 +247,102 @@ specified position."
  (uint32_t    sh_info) ;;  Additional section information 
  (uint32_t    sh_addralign) ;;  Section alignment 
  (uint32_t    sh_entsize)) ;;  Entry size if section holds table 
+
+(elfenum
+ ELF32_ST_BIND
+ :STB_LOCAL 0
+ :STB_GLOBAL 1
+ :STB_WEAK 2
+ :STB_LOPROC 13
+ :STB_HIPROC 15)
+
+(elfenum
+ ELF32_ST_TYPE
+:STT_NOTYPE 0
+:STT_OBJECT 1
+:STT_FUNC 2
+:STT_SECTION 3
+:STT_FILE 4
+:STT_LOPROC 13
+:STT_HIPROC 15)
+
+(defn parse-st_info [st_info]
+  (let [st_bind (bit-shift-right st_info 4)
+        st_type (bit-and st_info 0xf)
+        st_bind (to-enum-name ELF32_ST_BIND st_bind)
+        st_type (to-enum-name ELF32_ST_TYPE st_type)]
+    {:st_bind st_bind :st_type st_type}))
+
+(elf-struct32
+ Sym
+ (uint32_t st_name)
+ (ElfN_Addr st_value)
+ (uint32_t st_size)
+ (uchar st_info :convert parse-st_info)
+ (uchar st_other)
+ (uint16_t st_shndx :enum shnum_special_enum))
+
+(defn parse-r_info [r_info]
+  {:r_sym (bit-shift-right r_info 8)
+   :r_type (byte (bit-and r_info 0xff))})
+
+(elf-struct32
+ Rel
+ (ElfN_Addr r_offset)
+ (uint32_t r_info :convert parse-r_info))
+
+(elf-struct32
+ Rela
+ (ElfN_Addr r_offset)
+ (uint32_t r_info :convert parse-r_info)
+ (int32_t r_addend))
+
+(defn- load-elf-table*
+  ([type shdr] (load-elf-table* type shdr nil))
+  ([type {:keys [sh_entsize sh_offset sh_size] :as shdr} num-entries]
+     (let [num-entries
+           (or num-entries
+               (int (/ sh_size sh_entsize)))]
+       (jump-position
+        sh_offset
+        (doall
+         (for [i (range num-entries)]
+           (with-buffer (brbytes sh_entsize) (read-elf-struct type))))))))
+
+(defn- find-shdr [shdrs name]
+  (first (filter #(= (:name %) name) shdrs)))
+
+(defn- load-elf-sym-table [all-shdrs {:keys [sh_entsize sh_offset sh_size sh_link] :as shdr}]
+  (let [syms (load-elf-table* :Sym shdr)
+        strtab (nth all-shdrs sh_link)
+        str-offset (:sh_offset strtab)]
+    (doall
+     (for [sym syms] 
+       (let [st_name (:st_name sym)
+             sym-name
+             (when (and str-offset (not= st_name 0))
+               (brntstr (+ str-offset st_name)))]
+         (assoc sym :name sym-name))))))
+
+(defn- load-elf-rel-table [{:keys [] :as shdr}]
+  (load-elf-table* :Rel shdr))
+
+(defn- load-elf-rela-table [{:keys [] :as shdr}]
+  (load-elf-table* :Rela shdr))
+
+(defn- load-prog-bits [{:keys [sh_offset sh_size]}]
+  (brbytes sh_offset sh_size))
+
+(defn- load-elf-Shdr [all-shdrs {:keys [sh_type] :as shdr}]
+  (let [data
+        (case sh_type
+          :SHT_SYMTAB (load-elf-sym-table all-shdrs shdr)
+          :SHT_DYNSYM (load-elf-sym-table all-shdrs shdr)
+          :SHT_RELA (load-elf-rela-table shdr)
+          :SHT_REL (load-elf-rel-table shdr)
+          :SHT_PROGBITS (load-prog-bits shdr)
+          nil)]
+   (assoc shdr :data data)))
 
 (defn- read-elf-Shdr [ehdr]
   (let [{:keys [e_shentsize e_shnum e_shoff e_shstrndx]} ehdr]
@@ -213,19 +356,41 @@ specified position."
                    (read-elf-struct :Shdr)))))
             str-hdr (when (< e_shstrndx e_shnum)
                       (nth shdrs e_shstrndx))
-            str-offset (:sh_offset str-hdr)]
-        (doall
-         (for [{:keys [sh_name] :as shdr} shdrs]
-           (let [str-pos (+ str-offset sh_name)]
-             (assoc shdr :name (brntstr str-pos)))))))))
+            str-offset (:sh_offset str-hdr)
+            shdrs
+            (doall
+             (for [{:keys [sh_name] :as shdr} shdrs]
+               (let [str-pos (+ str-offset sh_name)]
+                 (assoc shdr :name (brntstr str-pos)))))]
+        (doall (map (partial load-elf-Shdr shdrs) shdrs))))))
  
 (defn read-elf32 [filename]
   (with-buffer filename
     (binding [*elf-arch* 32]
       (let [ehdr (read-elf-struct :Ehdr)
             shdrs (read-elf-Shdr ehdr)]
-        {:elf-header ehdr
-         :section-headers shdrs
-         :program-headers nil}))))
+        {:filename (Path/GetFullPath filename)
+         :elf-header ehdr
+         :sections shdrs
+         :program-header nil}))))
 
-(read-elf32 "c:/tmp/test1.o")
+(defn find-elf-section [elf name]
+  (find-shdr (:sections elf) name))
+
+(defn find-sym-table [elf]
+  (:data (find-elf-section elf ".symtab")))
+
+(defn find-global-symbols [elf]
+  (filter
+   (fn [sym] (= (get-in sym [:st_info :st_bind]) :STB_GLOBAL))
+   (find-sym-table elf)))
+
+(defn filter-defined-symbols [global-symbols]
+  (filter
+   (fn [sym] (not= (get-in sym [:st_shndx]) :SHN_UNDEF))
+   global-symbols))
+
+(defn filter-undefined-symbols [global-symbols]
+  (filter
+   (fn [sym] (= (get-in sym [:st_shndx]) :SHN_UNDEF))
+   global-symbols))
--- src/c_in_clj/gcc/remote_link.clj
+++ src/c_in_clj/gcc/remote_link.clj
@@ -1,9 +1,17 @@
 (ns c-in-clj.gcc.remote-link
-  (:require [c-in-clj.gcc.gdb :as gdb]))
+  (:require [c-in-clj.gcc.gdb :as gdb]
+            [c-in-clj.gcc.elf :as elf])
+  (:import
+   [System.IO Directory SearchOption]))
 
 (comment
   (defn read-memory [addr count]
     (gdb/gdb-sync-exec "-data-read-memory u 1 " addr " " count)))
+
+(def ^:private linker-data (atom {}))
+
+(defn- get-data []
+  (or (gdb/get-client-data) linker-data))
 
 (defn- write-memory* [addr data type multiplier]
   (doall
@@ -44,3 +52,84 @@
 
 (defn stepi []
   (gdb/gdb-sync-exec "-exec-step-instruction"))
+
+(defprotocol ILinkerTarget
+  (calc-rela [rela])
+  (alloc-mem [size]))
+
+(defn cache-obj-file [filename]
+  (let [elf (elf/read-elf32 filename)
+        global-symbols (elf/find-global-symbols elf)
+        defined-symbols (elf/filter-defined-symbols global-symbols)]
+    (doseq [{:keys [name] :as sym} defined-symbols]
+      (println "Caching" name)
+      (swap! (get-data) assoc-in [:symbol-cache name]
+             {:symdata sym :obj-file elf}))))
+
+(defn cache-obj-dir [path]
+  (doseq [filename (Directory/EnumerateFiles path "*.o" SearchOption/AllDirectories)]
+    (try
+      (cache-obj-file filename)
+      (catch Object ex
+        (println "Error" ex)))))
+
+
+(defn- find-sym-rel-sections [obj-file {:keys [st_shndx] :as symdata}]
+  (when (number? st_shndx)
+    (filter
+     (fn [{:keys [sh_type sh_info] :as section}]
+       (and (or (= :SHT_REL sh_type)
+                (= :SHT_RELA sh_type))
+            (= sh_info st_shndx)))
+     (:sections obj-file))))
+
+(defn- find-sym-section
+  ([{:keys [obj-file symdata]}]
+     (find-sym-section obj-file symdata))
+  ([obj-file {:keys [st_shndx] :as symdata}]
+     (when (number? st_shndx)
+       (nth (:sections obj-file) st_shndx))))
+
+(defn- resolve-sym [symbol-name]
+  (get-in @(get-data) [:symbol-cache symbol-name]))
+
+(defn- throw-unresolved-symbol [name]
+  (throw (ex-info
+          (str "Unresolved symbol " name)
+          {:type ::unresolved-symbol :name name})))
+
+(declare link-symbol)
+
+(defn- find-relocation-target [obj-file {:keys [st_shndx name] :as r_sym}]
+  (let [{:keys [st_bind st_type]} (:st_info r_sym)]
+    (cond
+     (and (= st_bind :STB_LOCAL)
+          (= st_type :STT_SECTION)
+          (number? st_shndx)) (find-sym-section obj-file r_sym)
+     (= :SHN_UNDEF st_type) (or
+                              (link-symbol name)
+                              (throw-unresolved-symbol name))
+     :default (throw-unresolved-symbol name))))
+
+(defn- resolve-rel-entries [obj-file {:keys [sh_link data] :as rel-section}]
+  (let [sym-tab (:data (nth (:sections obj-file) sh_link))]
+    (for [{:keys [r_info] :as rel-entry} data]
+      (let [r_sym (:r_sym r_info)
+            r_sym (nth sym-tab r_sym)
+            {:keys [st_bind st_type]} (:st_info r_sym)
+            rel-target (find-relocation-target obj-file r_sym)]
+        {:relocation-entry rel-entry
+         :relocation-symbol r_sym
+         :relocation-target rel-target}))))
+
+(defn link-symbol [symbol-name]
+  (println "Trying to link" symbol-name)
+  (let [{:keys [symdata obj-file]} (resolve-sym symbol-name)
+        sym-section (find-sym-section obj-file symdata)
+        rel-sections (find-sym-rel-sections obj-file symdata)
+        rel-entries (apply concat
+                           (map
+                            (partial resolve-rel-entries obj-file)
+                            rel-sections))]
+    sym-section))
+
