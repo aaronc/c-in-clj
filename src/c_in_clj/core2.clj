1c26a3a08885d504ff3b773006e96a6e3c1a1439
Aaron Craelius <aaroncraelius@gmail.com>
2013-01-07 13:07:32 -0500

    Work on type system.

--- src/c_in_clj/core2.clj
+++ src/c_in_clj/core2.clj
@@ -7,24 +7,120 @@
   (is-block? [this])
   (wrap-last [this func]))
 
+(defprotocol IField
+  (get-field-type [this])
+  (get-bitfield-width [this]))
+
+(defprotocol IType
+  (write-type [this])
+  (write-decl-expr [this var-name])
+  (is-reference-type? [this])
+  (create-new-expr [this args])
+  (common-denominator-type [this other-type])
+  (create-implicit-cast-expr [this expr])
+  (get-fields [this])
+  (create-field-access-expr [this instance-expr field-name]))
+
 (defmacro defliteral [name ctype]
-  `(defrecord ~name [value#]
+  (let [ctype (lookup-type ctype)]
+    `(defrecord ~name [value#]
        IExpression
        (write [_] (pr-str value#))
        (is-block? [_] false)
-       (get-type [_] 'ctype)))
+       (get-type [_] ~ctype))))
 
-(defliteral Int32Literal 'int32_t)
-(defliteral Int64Literal 'int64_t)
-(defliteral DoubleLiteral 'double)
-(defliteral BooleanLiteral 'bool)
-(defliteral StringLiteral 'char*)
+(defliteral Int32Literal int32_t)
+(defliteral Int64Literal int64_t)
+(defliteral DoubleLiteral double)
+(defliteral BooleanLiteral bool)
+(defliteral StringLiteral char*)
 
 (def null-literal
   (reify IExpression
-    (write [this] "0")
+    (write [this] "NULL")
     (is-block? [_] false)
     (get-type [this])))
+
+(def ^:private primitive-types (atom {}))
+
+(def ^:private type-aliases (atom {}))
+
+(defrecord PrimitiveType [type-name]
+  clojure.lang.Named
+  (getName [_] type-name)
+  IType
+  (write-type [_] type-name)
+  (write-decl-expr [_ var-name] (str type-name " " var-name))
+  (is-reference-type? [_] false)
+  (common-denominator-type [_ _]))
+
+(defmacro defprimitive [type-name]
+  (let [type-name (name type-name)]
+    `(swap! primitive-types
+            assoc
+            ~type-name
+            (PrimitiveType. ~type-name))))
+
+(defprimitive void)
+(defprimitive int8_t)
+(defprimitive int16_t)
+(defprimitive int32_t)
+(defprimitive int64_t)
+(defprimitive uint8_t)
+(defprimitive uint16_t)
+(defprimitive uint32_t)
+(defprimitive uint64_t)
+(defprimitive size_t)
+(defprimitive char)
+(defprimitive float)
+(defprimitive double)
+(defprimitive bool)
+
+(defmacro ctype-alias [type-alias type-name]
+  `(swap! type-aliases
+          assoc
+          ~(name type-alias)
+          ~(name type-name)))
+
+(ctype-alias i8 int8_t)
+(ctype-alias i16 int16_t)
+(ctype-alias i32 int32_t)
+(ctype-alias i64 int64_t)
+(ctype-alias u8 uint8_t)
+(ctype-alias u16 uint16_t)
+(ctype-alias u32 uint32_t)
+(ctype-alias u64 uint64_t)
+
+(defrecord PointerType [type]
+  clojure.lang.Named
+  (getName [_] (str (name type) "*"))
+  IType
+  (write-type [_] (str (write-type type) "*"))
+  (write-decl-expr [_ var-name] (str (write-type type) "* " var-name))
+  (is-reference-type? [_] true)
+  (get-fields [_]))
+
+(defrecord AnonymousType [type-name]
+  clojure.lang.Named
+  (getName [_] type-name)
+  IType
+  (write-type [_] type-name)
+  (write-decl-expr [_ var-name] (str type-name " " var-name))
+  (is-reference-type? [_] false))
+
+(defn lookup-type [type-name]
+  (cond
+   (satisfies? IType type-name) type-name
+   (keyword? type-name) (AnonymousType. (name type-name))
+   :default
+    (let [type-name (name type-name)]
+      (if-let [primitive (get @primitive-types type-name)]
+        primitive
+        (if-let [alias (get @type-aliases type-name)]
+          (lookup-type alias)
+          (if (.EndsWith type-name "*")
+            (PointerType. (lookup-type (.Substring type-name 0 (dec (.Length type-name)))))
+            (comment TODO)))))))
 
 (defprotocol IFunction
   (get-return-type [this]))
@@ -87,8 +183,6 @@
 
 (defn lookup-symbol [sym-name])
 
-(defn lookup-type [type-name])
-
 (defn lookup-macro [macro-name])
 
 (def ^:private cintrinsics (atom {}))
@@ -120,6 +214,7 @@
 
 (defn cexpand [form]
   (cond
+   (satisfies? IExpression form) form
    (nil? form) null-literal
    (number? form) (cexpand-num form)
    (= Boolean (type form)) (BooleanLiteral. form)
@@ -140,11 +235,10 @@
 
 (defmacro cop [sym args & body]
   `(cintrinsic ~sym ~args
-               (let [~@(reduce (fn [res x] (into res [x `(cexpand ~x)])) [] args)]
-                 (reify
-                   IExpression
-                   (is-block? [_] false)
-                   ~@body))))
+               (reify
+                 IExpression
+                 (is-block? [_] false)
+                 ~@body)))
 
 (defn get-bin-op-type [x y]
   (let [xt (get-type x)
@@ -327,20 +421,21 @@
 (defn cstatement [expr & {:keys [noindent]}]
   (Statement. (cexpand expr) noindent))
 
+(defrecord Statements [statements]
+  IExpression
+  (get-type [_] (get-type (last statements)))
+  (is-block? [_] true)
+  (write [_] (str/join "\n" (map write statements)))
+  (wrap-last [_ func]
+    (let [[statements last-st] statements]
+      (cstatements
+       (conj statements
+             (if (is-block? last-st)
+               (wrap-last last-st func)
+               (func last-st)))))))
+
 (defn cstatements [statements]
-  (let [statements (for [st statements] (cstatement st))]
-    (reify
-      IExpression
-      (get-type [_] (get-type (last statements)))
-      (is-block? [_] true)
-      (write [_] (str/join "\n" (map write statements)))
-      (wrap-last [_ func]
-        (let [[statements last-st] statements]
-          (cstatements
-           (conj statements
-                 (if (is-block? last-st)
-                   (wrap-last last-st func)
-                   (func last-st)))))))))
+  (Statements. (for [st statements] (cstatement st))))
 
 (defrecord CaseExpression [test cases]
   IExpression
@@ -434,6 +529,80 @@
                  (IfExpression. (cexpand expr)
                                 (cstatement then)
                                 (cstatement else)))))
+
+(defrecord DeclExpression [var-type var-name init-expr]
+  IExpression
+  (get-type [_] var-type)
+  (write [_] (str (write-decl-expr var-type var-name) "=" (when init-expr (write init-expr))))
+  (is-block? [_] false))
+
+(defrecord DeclRewriteExpression [decl-expr block-expr]
+  IExpression
+  (get-type [_] (get-type decl-expr))
+  (write [_]
+    (str (indent) (write decl-expr) ";\n"
+         (write block-expr)))
+  (is-block? [_] true)
+  (wrap-last [_ func] (wrap-last block-expr func)))
+
+(cintrinsic*
+ 'let
+ (fn [& [forms]]
+   (assert (even? (count forms)) "let expression must contain an even number of forms")
+   (apply
+    cstatements
+    (for [[decl expr-form] (partition 2 forms)]
+      (let [init-expr (cexpand expr-form)
+            explicit-tag (:tag (meta decl))
+            decl-type (if explicit-tag
+                        explicit-tag
+                        (get-type init-expr))]
+        (assert decl-type (str "unable to infer type for let binding: " decl " " expr-form))
+        (let [decl-type (lookup-type decl-type)]
+          (if (is-block? init-expr)
+            (let [init-expr (wrap-last init-expr (fn [x] (AssignExpression_set!. (comment TODO var ref expr) x)))]
+              (DeclRewriteExpression. (DeclExpression. decl-type (name decl) nil) init-expr))
+            (DeclExpression. decl-type (name decl) init-expr))))))))
+
+
+(def ^:private default-c-preamble
+  "#include <stddef.h>\n#include <stdint.h>\n#include <stdbool.h>\n")
+
+(def ^:private default-cpp-preamble
+  "#include <cstddef>\n#include <cstdint>\n")
+
+(defrecord StructField [name type-name bits]
+  clojure.lang.Named
+  (getName [_] name)
+  IField
+  (get-field-type [_] (lookup-type type-name))
+  (get-bitfield-width [_] bits))
+
+(defrecord Struct [name fields])
+
+(defn- print-struct [^Struct struct]
+  (println struct)
+  (str "typedef struct " (:name struct) " {\n"
+       (str/join
+        (for [field (:fields struct)]
+          (str " "
+               (write-decl-expr (get-field-type field)
+                                (name field))
+               (when-let [bits (get-bitfield-width field)]
+                 (str ":" bits)) ";\n")))
+       "} " (:name struct) ";"))
+
+(defn- compile-cstruct [struct-name members]
+  (let [struct
+        (Struct. (name struct-name)
+                 (for [[type-name field-name bits] members]
+                   (StructField. (name field-name) type-name bits)))]
+    (comment TODO add struct to module/package)
+    (print-struct struct)))
+
+(defmacro cstruct [name & members]
+  (compile-cstruct name members))
+
 
 
 
